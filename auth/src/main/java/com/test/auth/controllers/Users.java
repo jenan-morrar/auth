3390ac061cb6fc21924d20964bec6702255bd65c
Jenan Morrar <jenanmorrar06@gmail.com>
2022-03-26 21:24:37 +0300

    Authentication Project

---
+++ auth/src/main/java/com/test/auth/controllers/Users.java
@@ -0,0 +1,78 @@
+package com.test.auth.controllers;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.test.auth.models.User;
+import com.test.auth.services.UserService;
+import com.test.auth.validator.UserValidator;
+
+@Controller
+public class Users {
+    
+    private UserService userService;
+    
+    // NEW
+    private UserValidator userValidator;
+    
+    // NEW
+    public Users(UserService userService, UserValidator userValidator) {
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@Valid @ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @PostMapping("/registration")
+    public String registration(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session) {
+    	// NEW
+        userValidator.validate(user, result);
+    	if (result.hasErrors()) {
+            return "registrationPage.jsp";
+        }
+        userService.saveWithUserRole(user);
+        return "redirect:/login";
+    }
+    
+    @PostMapping("/login")
+    public String login(@RequestParam(value="error", required=false) String error, @RequestParam(value="logout", required=false) String logout, Model model) {
+        if(error != null) {
+            model.addAttribute("errorMessage", "Invalid Credentials, Please try again.");
+        }
+        if(logout != null) {
+            model.addAttribute("logoutMessage", "Logout Successful!");
+        }
+        return "loginPage.jsp";
+    }
+    @RequestMapping(value = {"/", "/home"})
+    public String home(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("currentUser", userService.findByUsername(username));
+        return "homePage.jsp";
+    }
+    
+    @RequestMapping("/admin")
+    public String adminPage(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("currentUser", userService.findByUsername(username));
+        return "adminPage.jsp";
+    }
+}
